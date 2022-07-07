fea6359e217d2faa27f1e9ddcb94652a467572ed
masaki-nakamori <gauss.1994.ilovephys@gmail.com>
2022-07-07 16:34:53 +0900

    新規投稿機能newとcreateを実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,20 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    #Strong Parameterの記述
+    Task.create(task_params)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
 end

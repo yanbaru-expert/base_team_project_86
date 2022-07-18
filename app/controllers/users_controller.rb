c6897ee11cba7cea64f268bf65ea4913b0c1d682
itaruby_on_rails <itaruo93o@icloud.com>
2022-07-18 12:36:42 +0700

    implement edit, update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
     @users = User.all
   end
@@ -12,20 +14,25 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    @user.update!(user_params)
   end
 
   def destroy
   end
 
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:name, :age)
   end
+
 end
